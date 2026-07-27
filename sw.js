ab51493d3df798476815e0b52318c46e977424b3
argonaft111111 <67hanamu@gmail.com>
2026-07-27 11:07:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,60 @@
+const CACHE_NAME = 'lifeos-v1';
 
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then(cached => {
+            if (cached) {
+              return cached;
+            }
+
+            if (
+              event.request.mode === 'navigate'
+            ) {
+              return caches.match('./index.html');
+            }
+
+            throw new Error('Offline');
+          });
+      })
+  );
+});
